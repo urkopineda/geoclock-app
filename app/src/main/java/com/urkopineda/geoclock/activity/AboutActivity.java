566ab9f61a0a7c2a84a6f1685e135acfafe5c369
Urko Pineda <urkopineda@gmail.com>
2016-11-19 21:57:22 +0100

    Edited main activity, added 'About' main activity and more
    Fixed Travis file and Codecov added

---
+++ app/src/main/java/com/urkopineda/geoclock/activity/AboutActivity.java
@@ -0,0 +1,20 @@
+package com.urkopineda.geoclock.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.urkopineda.geoclock.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+}
--- app/src/main/java/com/urkopineda/geoclock/activity/MainActivity.java
+++ app/src/main/java/com/urkopineda/geoclock/activity/MainActivity.java
@@ -1,16 +1,42 @@
 package com.urkopineda.geoclock.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.urkopineda.geoclock.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mainToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                startActivity(new Intent(this, AboutActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
